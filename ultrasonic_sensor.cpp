9c586315e8fe932794832bc269930fdfde5d9ded
Vladimir Voinea <voineavladimir@gmail.com>
2024-02-04 23:37:17 +0200

    Update ultrasonic_sensor constructor and method names

--- ultrasonic_sensor.cpp
+++ ultrasonic_sensor.cpp
@@ -3,13 +3,13 @@
 namespace ardent
 {
     ultrasonic_sensor::ultrasonic_sensor(pin echo_pin, pin trigger_pin)
-        : m_sr04(echo_pin, trigger_pin)
+        : m_sr04(echo_pin.get_pin_number(), trigger_pin.get_pin_number())
     {
     }
 
     std::optional<centimeters> ultrasonic_sensor::get_distance()
     {
-        const auto distance = m_sr04.get_distance();
+        const auto distance = m_sr04.Distance();
         return distance >= ultrasonic_sensor::min_distance && distance <= ultrasonic_sensor::max_distance ? {distance} : {};
     }
 }
