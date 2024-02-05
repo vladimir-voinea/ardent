378e2b11086c0b1facf16725bbfe98d12007aedb
Vladimir Voinea <voineavladimir@gmail.com>
2024-02-05 17:24:05 +0200

    Refactor passive buzzer

---
+++ passive_buzzer.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "digital_output_pin.hpp"
+#include "frequency.hpp"
+#include "i_loop_observer.hpp"
+#include <sleep.hpp>
+#include <time.hpp>
+#include <tuple>
+#include <functional>
+#include <optional>
+
+namespace ardent
+{
+    class passive_buzzer : public i_loop_observer
+    {
+    public:
+        passive_buzzer(digital_output_pin pin);
+
+        void beep(frequency freq, estd::milliseconds duration, std::function<void(void)> callback);
+        void mute(estd::milliseconds duration, std::function<void(void)> callback);
+
+        void update() override;
+        bool is_beeping() const;
+
+        void start_beeping(frequency freq);
+        void stop_beeping();
+    private:
+        digital_output_pin m_pin;
+        using current_beep_info = std::tuple<estd::milliseconds, estd::milliseconds>;
+        current_beep_info m_current_beep = {{0}, {0}};
+        current_beep_info m_current_mute = {{0}, {0}};
+        std::function<void(void)> m_callback;
+    };
+}
