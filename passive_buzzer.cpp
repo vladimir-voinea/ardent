b2929d3853b633ccc5f68f4350f6bdbab295bbaa
Vladimir Voinea <voineavladimir@gmail.com>
2024-02-05 16:57:06 +0200

    Add current_beep_info struct and update method

--- passive_buzzer.cpp
+++ passive_buzzer.cpp
@@ -11,15 +11,31 @@ namespace ardent
     void passive_buzzer::beep(frequency freq, estd::milliseconds duration)
     {
         ::tone(m_pin.get_pin_number(), freq, duration.value);
+        m_current_beep = current_beep_info{ardent::millis(), duration};
     }
 
     void passive_buzzer::start_beeping(frequency freq)
     {
         ::tone(m_pin.get_pin_number(), freq);
+        m_current_beep = current_beep_info{ardent::millis(), 0};
     }
 
     void passive_buzzer::stop_beeping()
     {
         ::noTone(m_pin.get_pin_number());
+        m_current_beep = {};
+    }
+
+    void passive_buzzer::update()
+    {
+        if(m_current_beep.has_value())
+        {
+            const auto& [start_time, duration] = m_current_beep.value();
+            if(ardent::millis().value - start_time.value > duration.value)
+            {
+                stop_beeping();
+            }
+        }
+        return i_loop_observer::action::none;
     }
 }
--- passive_buzzer.hpp
+++ passive_buzzer.hpp
@@ -2,20 +2,28 @@
 
 #include "digital_output_pin.hpp"
 #include "frequency.hpp"
+#include "i_loop_observer.hpp"
+#include <sleep.hpp>
 #include <time.hpp>
+#include <utility>
 
 namespace ardent
 {
-    class passive_buzzer
+    class passive_buzzer : public i_loop_observer
     {
     public:
         passive_buzzer(digital_output_pin pin);
 
         void beep(frequency freq, estd::milliseconds duration);
 
+        void update() override;
+        bool is_beeping() const { return m_current_beep.has_value(); }
+
         void start_beeping(frequency freq);
         void stop_beeping();
     private:
         digital_output_pin m_pin;
+        using current_beep_info = std::tuple<estd::milliseconds, estd::milliseconds>;
+        std::optional<current_beep_info> m_current_beep;
     };
 }
