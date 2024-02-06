cbcfb6044b899de01b17da086cede616fbb69c5d
Vladimir Voinea <voineavladimir@gmail.com>
2024-02-06 23:41:47 +0200

    Fix mute condition in passive_buzzer class

--- passive_buzzer.cpp
+++ passive_buzzer.cpp
@@ -9,7 +9,7 @@ passive_buzzer::passive_buzzer(digital_output_pin pin) : m_pin(pin)
 
 void passive_buzzer::beep(frequency freq, estd::milliseconds duration, std::function<void(void)> callback)
 {
-    if (std::get<0>(m_current_mute).value)
+    if (std::get<0>(m_current_mute).value || std::get<0>(m_current_mute).value == ~0u)
     {
         return;
     }
@@ -21,7 +21,7 @@ void passive_buzzer::beep(frequency freq, estd::milliseconds duration, std::func
 
 void passive_buzzer::start_beeping(frequency freq)
 {
-    if (std::get<0>(m_current_mute).value)
+    if (std::get<0>(m_current_mute).value || std::get<0>(m_current_mute).value == ~0u)
     {
         return;
     }
@@ -77,12 +77,12 @@ void passive_buzzer::mute(estd::milliseconds duration, std::function<void(void)>
 
 void passive_buzzer::mute()
 {
-    m_current_mute = m_current_mute{~0, estd::milliseconds{0}};
+    m_current_mute = {estd::milliseconds{~0u}, estd::milliseconds{0}};
 }
 
 void passive_buzzer::unmute()
 {
-    m_current_mute = m_current_mute{0, estd::milliseconds{0}};
+    m_current_mute = {estd::milliseconds{0}, estd::milliseconds{0}};
 }
 
 bool passive_buzzer::is_beeping() const
