529600523e8c7d278ee54389a7ce80684a4b5c7d
Vladimir Voinea <voineavladimir@gmail.com>
2024-02-07 23:16:01 +0200

    Add time unit conversion specializations

--- time.hpp
+++ time.hpp
@@ -30,124 +30,110 @@ struct hours
 {
     long unsigned int value;
 };
+// Specializations for converting between time units
 
-template <typename To, typename From> To time_cast(From from)
-{
-    if constexpr (std::is_same<To, microseconds>::value)
-    {
-        if constexpr (std::is_same<From, microseconds>::value)
-        {
-            return from;
-        }
-        else if constexpr (std::is_same<From, milliseconds>::value)
-        {
-            return {from.value * 1000};
-        }
-        else if constexpr (std::is_same<From, seconds>::value)
-        {
-            return {from.value * 1000000};
-        }
-        else if constexpr (std::is_same<From, minutes>::value)
-        {
-            return {from.value * 60000000};
-        }
-        else if constexpr (std::is_same<From, hours>::value)
-        {
-            return {from.value * 3600000000};
-        }
-    }
-    else if constexpr (std::is_same<To, milliseconds>::value)
-    {
-        if constexpr (std::is_same<From, microseconds>::value)
-        {
-            return {from.value / 1000};
-        }
-        else if constexpr (std::is_same<From, milliseconds>::value)
-        {
-            return from;
-        }
-        else if constexpr (std::is_same<From, seconds>::value)
-        {
-            return {from.value * 1000};
-        }
-        else if constexpr (std::is_same<From, minutes>::value)
-        {
-            return {from.value * 60000};
-        }
-        else if constexpr (std::is_same<From, hours>::value)
-        {
-            return {from.value * 3600000};
-        }
-    }
-    else if constexpr (std::is_same<To, seconds>::value)
-    {
-        if constexpr (std::is_same<From, microseconds>::value)
-        {
-            return {from.value / 1000000};
-        }
-        else if constexpr (std::is_same<From, milliseconds>::value)
-        {
-            return {from.value / 1000};
-        }
-        else if constexpr (std::is_same<From, seconds>::value)
-        {
-            return from;
-        }
-        else if constexpr (std::is_same<From, minutes>::value)
-        {
-            return {from.value * 60};
-        }
-        else if constexpr (std::is_same<From, hours>::value)
-        {
-            return {from.value * 3600};
-        }
-    }
-    else if constexpr (std::is_same<To, minutes>::value)
-    {
-        if constexpr (std::is_same<From, microseconds>::value)
-        {
-            return {from.value / 60000000};
-        }
-        else if constexpr (std::is_same<From, milliseconds>::value)
-        {
-            return {from.value / 60000};
-        }
-        else if constexpr (std::is_same<From, seconds>::value)
-        {
-            return {from.value / 60};
-        }
-        else if constexpr (std::is_same<From, minutes>::value)
-        {
-            return from;
-        }
-        else if constexpr (std::is_same<From, hours>::value)
-        {
-            return {from.value * 60};
-        }
-    }
-    else if constexpr (std::is_same<To, hours>::value)
-    {
-        if constexpr (std::is_same<From, microseconds>::value)
-        {
-            return {from.value / 3600000000};
-        }
-        else if constexpr (std::is_same<From, milliseconds>::value)
-        {
-            return {from.value / 3600000};
-        }
-        else if constexpr (std::is_same<From, seconds>::value)
-        {
-            return {from.value / 3600};
-        }
-        else if constexpr (std::is_same<From, minutes>::value)
-        {
-            return {from.value / 60};
-        }
-        else if constexpr (std::is_same<From, hours>::value)
-        {
-            return from;
-        }
-    }
-    return {};
+// General template declaration
+template <typename To, typename From>
+To time_cast(From from);
+
+// microseconds to other units
+template <>
+milliseconds time_cast<milliseconds, microseconds>(microseconds from) {
+    return {from.value / 1000};
 }
+
+template <>
+seconds time_cast<seconds, microseconds>(microseconds from) {
+    return {from.value / 1000000};
+}
+
+template <>
+minutes time_cast<minutes, microseconds>(microseconds from) {
+    return {from.value / 60000000};
+}
+
+// milliseconds to other units
+template <>
+microseconds time_cast<microseconds, milliseconds>(milliseconds from) {
+    return {from.value * 1000};
+}
+
+template <>
+seconds time_cast<seconds, milliseconds>(milliseconds from) {
+    return {from.value / 1000};
+}
+
+template <>
+minutes time_cast<minutes, milliseconds>(milliseconds from) {
+    return {from.value / 60000};
+}
+
+template <>
+hours time_cast<hours, milliseconds>(milliseconds from) {
+    return {from.value / 3600000};
+}
+
+// seconds to other units
+template <>
+microseconds time_cast<microseconds, seconds>(seconds from) {
+    return {from.value * 1000000};
+}
+
+template <>
+milliseconds time_cast<milliseconds, seconds>(seconds from) {
+    return {from.value * 1000};
+}
+
+template <>
+minutes time_cast<minutes, seconds>(seconds from) {
+    return {from.value / 60};
+}
+
+template <>
+hours time_cast<hours, seconds>(seconds from) {
+    return {from.value / 3600};
+}
+
+// minutes to other units
+template <>
+microseconds time_cast<microseconds, minutes>(minutes from) {
+    return {from.value * 60000000};
+}
+
+template <>
+milliseconds time_cast<milliseconds, minutes>(minutes from) {
+    return {from.value * 60000};
+}
+
+template <>
+seconds time_cast<seconds, minutes>(minutes from) {
+    return {from.value * 60};
+}
+
+template <>
+hours time_cast<hours, minutes>(minutes from) {
+    return {from.value / 60};
+}
+
+
+template <>
+milliseconds time_cast<milliseconds, hours>(hours from) {
+    return {from.value * 3600000};
+}
+
+template <>
+seconds time_cast<seconds, hours>(hours from) {
+    return {from.value * 3600};
+}
+
+template <>
+minutes time_cast<minutes, hours>(hours from) {
+    return {from.value * 60};
+}
+
+// Note: This set of specializations assumes that each 'value' field is of a type
+// compatible with the conversion being performed (e.g., long long).
+
+// You need to replace `// Add conversion logic here` with the actual conversion logic as shown in the examples.
+
 } // namespace estd
